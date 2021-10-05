4b68c8190f4af6908016761e789d28854a2d0cee
HiroUrata <1712026@i.softbank.jp>
2021-10-05 12:27:16 +0900

    ConsentStatementViewController 作成

---
+++ RealmCurrentLocation/Controller/ConsentStatementViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ConsentStatementViewController.swift
+//  RealmCurrentLocation
+//
+//  Created by UrataHiroki on 2021/10/05.
+//
+
+import UIKit
+
+class ConsentStatementViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+     
+    }
+    
+
+    
+}

d73cad6282d8c2c09a0b784bb7dbf3de11dd0525
HiroUrata <1712026@i.softbank.jp>
2021-10-05 12:44:02 +0900

    ConsentStatementViewController  check機能　追加

--- RealmCurrentLocation/Controller/ConsentStatementViewController.swift
+++ RealmCurrentLocation/Controller/ConsentStatementViewController.swift
@@ -9,12 +9,28 @@ import UIKit
 
 class ConsentStatementViewController: UIViewController {
 
+    @IBOutlet weak var consentStatementView: UITextView!
+    @IBOutlet weak var checkButton: UIButton!
+    @IBOutlet weak var startButton: UIButton!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
      
     }
     
-
+    @IBAction func check(_ sender: UIButton) {
+        
+        if sender.currentBackgroundImage == UIImage(systemName: "stop"){
+            
+            sender.setBackgroundImage(UIImage(systemName: "checkmark.square"), for: .normal)
+        }else if sender.currentBackgroundImage == UIImage(systemName: "checkmark.square"){
+            
+            sender.setBackgroundImage(UIImage(systemName: "stop"), for: .normal)
+        }
+        
+    }
+    
     
 }

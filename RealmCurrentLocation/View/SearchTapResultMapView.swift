f40c793b3678d5e1e41dbbd4b33481c9eb875b56
HiroUrata <1712026@i.softbank.jp>
2021-10-10 11:50:49 +0900

    SearchTapResultMapView 作成

---
+++ RealmCurrentLocation/View/SearchTapResultMapView.swift
@@ -0,0 +1,13 @@
+//
+//  SearchTapResultMapView.swift
+//  RealmCurrentLocation
+//
+//  Created by UrataHiroki on 2021/10/10.
+//
+
+import UIKit
+
+class SearchTapResultMapView:UIViewController{
+    
+    
+}
